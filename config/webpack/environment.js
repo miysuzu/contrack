fea52dd18d190a7b3fc05836111376ff3b7160a2
EC2 Default User <ec2-user@ip-172-31-35-218.ap-northeast-1.compute.internal>
2025-07-28 18:04:38 +0000

    Webpacker構成に統一（Sass移行・不要CSS削除・package.json整備）

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,4 +10,19 @@ environment.plugins.prepend('Provide',
   })
 )
 
+// CSSとSassの処理を有効化
+const sassLoader = environment.loaders.get('sass')
+sassLoader.use = sassLoader.use.map(loader => {
+  if (loader.loader === 'sass-loader') {
+    return {
+      ...loader,
+      options: {
+        ...loader.options,
+        implementation: require('sass')
+      }
+    }
+  }
+  return loader
+})
+
 module.exports = environment
