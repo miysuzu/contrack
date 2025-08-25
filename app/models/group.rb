b4d5a5247c2c13f08e0e477586763129465cc417
EC2 Default User <ec2-user@ip-172-31-35-218.ap-northeast-1.compute.internal>
2025-08-25 11:01:54 +0000

    ”最終調整7”

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,8 @@ class Group < ApplicationRecord
   has_many :contracts, dependent: :destroy
   has_many :group_join_requests, dependent: :destroy
   has_many :pending_join_requests, -> { where(status: :pending) }, class_name: 'GroupJoinRequest'
+
+  # バリデーション
+  validates :name, presence: { message: 'を入力してください' }, 
+                   length: { minimum: 1, maximum: 50, message: 'は1文字以上50文字以下で入力してください' }
 end
